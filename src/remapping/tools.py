16b5e1735abfd3406757b2b9c03d97d9adb838a3
AleksanderPawlak <aleksander.pawlak1996@gmail.com>
2021-09-08 23:09:54 +0200

    second testcase

--- src/remapping/tools.py
+++ src/remapping/tools.py
@@ -7,12 +7,12 @@ import pathlib
 class System(enum.Enum):
     WINDOWS = enum.auto()
     POSIX = enum.auto()
+    # TODO: LINUX and Mac instead of POSIX
 
 
-def remap(
-    mapping: typing.Dict[str, str], input_paths: typing.List[str], platform: System
-) -> typing.List[str]:
+def get_path_resolver(platform: System) -> typing.Type[pathlib.PurePath]:
     # TODO: maybe without PurePathLib?
+    # Can PureWindowsPath deal with all kinds of paths? What about relative paths
     path_resolver = {
         System.WINDOWS: pathlib.PureWindowsPath,
         System.POSIX: pathlib.PurePosixPath,
@@ -20,15 +20,58 @@ def remap(
     if path_resolver is None:
         raise ValueError(f"Passed platform: {platform} is not supported.")
 
+    return path_resolver
+
+
+def remap(
+    mapping: typing.Dict[str, str], input_paths: typing.List[str], platform: System
+) -> typing.List[str]:
+    path_resolver = get_path_resolver(platform)
     result = []
 
     # FIXME: This loop could be probably better.
     for input_path in input_paths:
+        # FIXME: key, value <- rename
         for key, value in mapping.items():
             if path_resolver(key) in path_resolver(input_path).parents:
                 result_path = value + input_path[len(key) :]
                 result.append(result_path)
                 break
+        else:
+            result.append(input_path)
+
+    return result
+
+
+# TODO: maybe these two functions should be merged?
+def remap_cross_platform(
+    mapping: typing.Dict[System, typing.Iterable[str]],
+    input_paths: typing.List[str],
+    dst_platform: System,
+) -> typing.List[str]:
+    dst_paths = mapping.pop(dst_platform)
+    dst_resolver = get_path_resolver(dst_platform)
+    result = []
+    # FIXME: ugly nested loops and breaks
+    for input_path in input_paths:
+        for platform, paths in mapping.items():
+            path_resolver = get_path_resolver(platform)
+            for idx, path in enumerate(paths):
+                resolved_input_path = path_resolver(input_path)
+                if path_resolver(path) in resolved_input_path.parents:
+                    resolved_input_path = (
+                        resolved_input_path.as_posix()
+                        if dst_platform == System.POSIX
+                        else str(resolved_input_path)
+                    )
+                    result_path = dst_resolver(dst_paths[idx]).joinpath(
+                        resolved_input_path[len(path) :]
+                    )
+                    result.append(str(result_path))
+                    break
+            else:
+                continue
+            break
         else:
             result.append(input_path)
 
--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -4,6 +4,7 @@ from src import remapping
 
 
 class TestMapping(unittest.TestCase):
+    # TODO: test situation where all windows paths are relative and written with "\" separator
     def test_remap_paths_from_windows(self):
         # TODO: think about other input data format
         input_mapping = {
@@ -53,12 +54,38 @@ class TestMapping(unittest.TestCase):
 
         self.assertEqual(expected_result, result)
 
-    # def test_remap_paths_from_different_platforms(self):
-    #     ...
+    def test_remap_paths_from_different_platforms(self):
+        input_mapping = {
+            remapping.tools.System.WINDOWS: ["L:\\", "P:\\"],
+            remapping.tools.System.POSIX: ["/Volumes/storage1", "/Volumes/storage2"],
+        }
+        input_paths = [
+            "L:\\temp",
+            "p:/project1/textures\\grass.tga",
+            "P:\\project1\\assets\\env\\Forest",
+            "cache\\Tree.abc",
+            "g:\\nope",
+        ]
+        expected_result = [
+            "/Volumes/storage1/temp",
+            "/Volumes/storage2/project1/textures/grass.tga",
+            "/Volumes/storage2/project1/assets/env/Forest",
+            # "cache/Tree.abc",  # This might be a bug in specification
+            "cache\\Tree.abc",
+            "g:\\nope",
+        ]
+        result = remapping.tools.remap_cross_platform(
+            input_mapping, input_paths, remapping.tools.System.POSIX
+        )
+
+        self.assertEqual(expected_result, result)
 
     # def test_remap_from_mixed_platforms(self):
     #     ...
 
     # TODO: test other failing cases
     # def test_remap_mixed_platforms_with_different_paths_number_should_raise_error(self):
+    #     ...
+
+    # def test_incorrect_remap_path_types(self):
     #     ...
